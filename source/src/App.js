b35d3ea0daff86312fadc7346cf5968bc1f49643
Eric Zhang <ericspring08@gmail.com>
2021-09-25 11:02:48 -0500

    version 1

--- source/src/App.js
+++ source/src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import {AppBar} from '@material-ui/core';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [dog_url, setDog] = useState("");
+
+  useEffect(() => {
+    async function fetchData(){
+      let response = await fetch("https://random.dog/woof.json");
+      let data = await response.json()
+      return data;
+    }
+
+    fetchData().then(data => {
+      setDog(data.url);
+      console.log(data.url);
+    })
+  }, [])
+
+  const DogPicture = () => {
+    if(dog_url.endsWith('.mp4')){
+      return(
+        <div>
+          <video height={400} controls autoPlay>
+            <source src={dog_url} type="video/mp4"/>
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )
+    }else {
+      return(
+        <img height={400} src={dog_url}></img>
+      )
+    }
+  }
   return (
     <div className="App">
-      
+      <AppBar position="sticky">
+        <h1>Dog Pictures</h1>
+      </AppBar>
+      <div style = {{margin: 50}}>
+        <DogPicture></DogPicture>
+      </div>
     </div>
   );
 }
